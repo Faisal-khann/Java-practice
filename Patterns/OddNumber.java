a7eac53f93dd088cbf748508e992f84fb5a14d82
Faisal-Khann <faisalkhanfaisal919@gmail.com>
2024-04-16 20:32:48 +0530

    git add -A

--- Patterns/OddNumber.java
+++ Patterns/OddNumber.java
@@ -6,9 +6,6 @@ public class OddNumber {
         System.out.print("Enter the value of rows:");
         int n = sc.nextInt();
         for (int i = 1; i<=n; i++){
-            for (int k = 1; k<n-1; k++){
-                System.out.print(" ");
-            }
             for (int j = 1; j<=2*i-1; j++){
                 System.out.print(j+" ");
                 // System.out.print("*");
