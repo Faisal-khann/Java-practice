cac84d01dcf31a4eba8f82a3f7e091a049559b22
Faisal-Khann <faisalkhanfaisal919@gmail.com>
2024-04-16 17:44:04 +0530

    git add -A

--- Arrays/KadaneAlgorithm.java
+++ Arrays/KadaneAlgorithm.java
@@ -1,22 +1,25 @@
-//Maximum sum sub Arrays
+//Maximum sum sub Arrays for both positive and negative test cases
 public class KadaneAlgorithm {
-    static int maxSumSubArray(int[]arr) {
+    static int maxSumSubArray(int[] arr, int n) {
+        // Max_so_far = maxSum
+        // Max_ending_here = curSum
         int maxSum = 0;
         int curSum = 0;
-        for (int i = 0; i < arr.length; i++) {
+        for (int i = 0; i < n; i++) {
             curSum = curSum + arr[i];
-            if(curSum > maxSum) {
-                maxSum = curSum;
+            if (curSum < arr[i]) {
+                curSum = arr[i];
             }
-            // Check for negative element
-            if(curSum < 0){
-                curSum = 0;
+            if (maxSum < curSum) {
+                maxSum = curSum;
             }
         }
         return maxSum;
     }
+
     public static void main(String[] args) {
-        int[]arr = {5, -4, -2, 6, -1};
-        System.out.println("Maximum Sum of Sub Array is:"+maxSumSubArray(arr));
+        int[] arr = { 5, -4, -2, 6, -1 };
+        int n = arr.length;
+        System.out.println("Maximum Sum of Sub Array is:" + maxSumSubArray(arr, n));
     }
 }
