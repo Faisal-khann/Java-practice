484e3d873bc0d34548778d1dcaa354fa7992bf02
Faisal-Khann <faisalkhanfaisal919@gmail.com>
2024-04-20 14:46:33 +0530

    git commit -A

---
+++ Maths/PrimeNum.java
@@ -0,0 +1,31 @@
+
+//Prime Number is number which is divisible by 1 and its-self.
+//Time complexity is : 0(n)
+package Maths;
+import java.util.*;
+
+public class PrimeNum {
+    // Create Method for primeNumber
+    public static void primeNumber() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the value of Number: ");
+        int num = sc.nextInt();
+        int temp = 0;
+        for (int i = 2; i <= num - 1; i++) { // Check number is divisible by other number or not
+            if (num % i == 0) {
+                temp = temp + 1;
+            }
+        }
+
+        if (temp > 0) {
+            System.out.println("Number is not prime. ");
+        } else {
+            System.out.println("Number is Prime.");
+        }
+        sc.close();
+    }
+
+    public static void main(String[] args) {
+        primeNumber();
+    }
+}
