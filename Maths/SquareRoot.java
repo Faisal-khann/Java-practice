6b16d969ba05769b4ae1d9ee1d0dbe0bf6ead7e4
Faisal-Khann <faisalkhanfaisal919@gmail.com>
2024-04-26 01:22:14 +0530

    git add -A

--- Maths/SquareRoot.java
+++ Maths/SquareRoot.java
@@ -1,16 +1,23 @@
 import java.util.*;
-import java.lang.*; //lang use for mathematics calculation
+// import java.lang.*; //lang use for mathematics calculation
 public class SquareRoot {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.println("Enter the value of n: ");
-        int n = input.nextInt();
+    public static void findSquareRoot(){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the value of n: ");
+        int n = sc.nextInt();
          //calculating the square of n
-        int sqr = (int) Math.pow(n, 2);
-        System.out.println("Square of the number "+n+ " is "+sqr+" ");
+         int sqr = (int) Math.pow(n, 2);
+         System.out.println("Square of the number "+ n + " is: "+sqr+" ");
+ 
+         //calculating the cube of n
+         int cube = (int) Math.pow(n, 3);
+         System.out.println("Cube of the number  "+ n +" is: " +cube);
 
-        //calculating the cube of n
-        int cube = (int) Math.pow(n, 3);
-        System.out.println("Cube of the number  "+n+" is " +cube);
+         sc.close();
+    }
+    public static void main(String[] args) {
+        findSquareRoot();
+    
+    
     }
 }
