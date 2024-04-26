e58a78518000d73a2760620fb567936a6412ba81
Faisal-Khann <faisalkhanfaisal919@gmail.com>
2024-04-27 00:54:59 +0530

    git add -A

--- Maths/SwappingNum.java
+++ Maths/SwappingNum.java
@@ -1,20 +1,26 @@
 import java.util.Scanner;
-public class SwappingNum {
-    public static void main(String[] args) {
-        Scanner Sc = new Scanner(System.in);
-        System.out.println("Enter the value of num1");
-        int num1 = Sc.nextInt();
-        System.out.println("Enter the value of num2");
-        int num2 = Sc.nextInt();
-        System.out.println("Before Swapping : num1 = "+num1+ " & num2 = "+num2);
 
-        //Swapping num1 and num2 by using temp
+public class SwappingNum {
+    public static void mySwap() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the value of num1");
+        int num1 = sc.nextInt();
+        System.out.println("Enter the value of num2");
+        int num2 = sc.nextInt();
+        System.out.println("Before Swapping : num1 = " + num1 + " & num2 = " + num2);
+
+        // Swapping num1 and num2 by using temp
         int temp = 0;
-       temp= num1;
-       num1=num2;
-       num2=temp;
-       System.out.println("After Swapping : num1 = "+num1+" & num2 = "+num2);
-     
-       Sc.close();
+        temp = num1;
+        num1 = num2;
+        num2 = temp;
+        System.out.println("After Swapping : num1 = " + num1 + " & num2 = " + num2);
+
+        sc.close();
+    }
+
+    public static void main(String[] args) {
+        mySwap();
+
     }
 }
