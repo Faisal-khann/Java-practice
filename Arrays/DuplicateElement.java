b3af189756a3b82d0ccdb643299fe33a5c3ca52c
Faisal-Khann <faisalkhanfaisal919@gmail.com>
2024-05-10 11:25:02 +0530

    git add -A

--- Arrays/DuplicateElement.java
+++ Arrays/DuplicateElement.java
@@ -1,18 +1,19 @@
 public class DuplicateElement {
-    public static void findDuplicate(int[]arr){
+    public static void findDuplicate(int[] arr) {
         int n = arr.length;
         System.out.print("Duplicate Element is: ");
-        for (int i = 0; i<n; i++){
-            for (int j = i+1; j<n; j++){
-                if (arr[i]==arr[j])
-                    System.out.print(arr[j]+ " ");
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (arr[i] == arr[j])
+                    System.out.print(arr[j] + " ");
             }
         }
     }
+
     public static void main(String[] args) {
-        int[]arr = {1, 2, 2, 3, 3, 4};
+        int[] arr = { 1, 2, 2, 3, 3, 4 };
         findDuplicate(arr);
     }
 }
-//print duplicate element by using two loop when 2 duplicate is present
-//first loop for select element and second loop for iteration
+// print duplicate element by using two loop when 2 duplicate is present
+// first loop for select element and second loop for iteration
