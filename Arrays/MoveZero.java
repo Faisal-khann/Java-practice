e8cfac16b59e6f11156fbeb158a636914485896a
Faisal-Khann <faisalkhanfaisal919@gmail.com>
2024-05-18 15:41:54 +0530

    git add -A

--- Arrays/MoveZero.java
+++ Arrays/MoveZero.java
@@ -1,27 +1,29 @@
+
 //Program of Move zero at the end of the element
 //[0, 2, 0, 4, 5] -> [2, 4, 5, 0, 0] 
 import java.util.*;
+
 public class MoveZero {
-    static void moveZero(int[]arr, int n){
+    static void moveZero(int[] arr, int n) {
         int zero = 0;
-        for(int i = 0; i < n; i++){
-            if(arr[i]==0){
-                zero ++ ;
-            }
-            else{
+        for (int i = 0; i < n; i++) {
+            if (arr[i] == 0) {
+                zero++;
+            } else {
                 int temp = arr[i];
                 arr[i] = 0;
-                arr[i-zero] = temp;  
+                arr[i - zero] = temp;
             }
         }
     }
+
     public static void main(String[] args) {
-        int[]arr = {0, 2, 0, 4, 5};
+        int[] arr = { 0, 2, 0, 4, 5 };
         int n = arr.length;
-        System.out.println("Before Moving: "+Arrays.toString(arr));
+        System.out.println("Before Moving: " + Arrays.toString(arr));
         moveZero(arr, n);
-        System.out.println("After Moving: "+ Arrays.toString(arr));
-        
+        System.out.println("After Moving: " + Arrays.toString(arr));
+
     }
-    
+
 }
