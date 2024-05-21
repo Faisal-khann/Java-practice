56cb405317a25ec5591d1e279ac3d64a59a2762b
Faisal-Khann <faisalkhanfaisal919@gmail.com>
2024-05-21 20:44:28 +0530

    git add -A

--- Arrays/MaximumElement.java
+++ Arrays/MaximumElement.java
@@ -1,24 +1,27 @@
 import java.util.Arrays;
 public class MaximumElement {
 //    This Method take less time complexity and the time complexity is o(n)
-//    public static int max(int[]arr){
-//        int maxVal = arr[0]; //Assume Max value in 0th index
-//        for (int i = 1; i< arr.length; i++){
-//            if (arr[i]>maxVal){
-//                maxVal = arr[i];
-//            }
-//        }
-//        return maxVal;
-//    }
+     static int findMaxElement(int[]arr){
+       int maxVal = arr[0]; //Assume Max value in 0th index
+
+       for (int i = 1; i< arr.length; i++){
+           if (arr[i]>maxVal){
+               maxVal = arr[i];
+           }
+       }
+       return maxVal;
+   }
     public static void main(String[] args) {
         int[]arr = {4, 16, 12, 20, 8 };
         System.out.println("Original Array: "+Arrays.toString(arr));
-//        System.out.println("Given Arrays: "+ Arrays.toString(arr));
-//        System.out.println("Maximum element: "+max(arr));
-        Arrays.sort(arr);
-        System.out.println("After Sorting: "+ Arrays.toString(arr));
-        int maxVal = arr[arr.length-1];
-        System.out.println("Maximum Element is: "+maxVal);
+       System.out.println("Given Arrays: "+ Arrays.toString(arr));
+       System.out.println("Maximum element: "+ findMaxElement(arr));
+
+       /*Using Arrays.sort() Method and Time complexity-> 0(nlogn)*/
+        /*  Arrays.sort(arr);
+            System.out.println("After Sorting: "+ Arrays.toString(arr));
+            int maxVal = arr[arr.length-1]; // take maxVal 
+            System.out.println("Maximum Element is: "+maxVal); */
     }
 }
 //method 1 : First we sort the Arrays by using sort() method then set the index of max element
