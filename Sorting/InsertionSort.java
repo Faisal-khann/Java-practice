a75da15c2aa6d5528d08c301e9b9dd3d8aae2198
Faisal-Khann <faisalkhanfaisal919@gmail.com>
2024-06-02 12:55:56 +0530

    git add -A

--- Sorting/InsertionSort.java
+++ Sorting/InsertionSort.java
@@ -1,14 +1,13 @@
 import java.util.Arrays;
 
 public class InsertionSort {
-    public static void insertionSort(int[] arr) {
-        int n = arr.length;
+    public static void insertionSort(int[] arr, int n) {
         for (int i = 1; i < n; i++) { // Run loop on unsorted part
             int key = arr[i]; // First element of unsorted part ex:- '11'
             int j = i - 1; // This is used for previous element of key value ex:- '12';
 
             while (j >= 0 && arr[j] > key) { // Used for comparison of j and key value 0
-                arr[j + 1] = arr[j]; // Swap
+                arr[j] = arr[j+1]; // Swap
                 j--;
             }
             arr[j + 1] = key;
@@ -17,8 +16,10 @@ public class InsertionSort {
 
     public static void main(String[] args) {
         int[] arr = { 12, 11, 13, 5, 6 };
+        int n = arr.length;
+
         System.out.println("Arrays Before sorting:" + Arrays.toString(arr));
-        insertionSort(arr);
+        insertionSort(arr, n);
         System.out.println("Arrays After sorting: " + Arrays.toString(arr));
     }
 }
