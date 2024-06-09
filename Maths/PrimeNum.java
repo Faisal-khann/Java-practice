ac8f26243933b53a493ceb62f9b822db596fbab1
Faisal-Khann <faisalkhanfaisal919@gmail.com>
2024-06-09 17:02:10 +0530

    git add -A

--- Maths/PrimeNum.java
+++ Maths/PrimeNum.java
@@ -1,28 +1,30 @@
+
 //Prime Number is number which is divisible by 1 and its-self.
 //Time complexity is : 0(n)
 import java.util.*;
+
 public class PrimeNum {
     // Create Method for primeNumber
     public static void primeNumber() {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the value of Number: ");
         int num = sc.nextInt();
-        int temp = 0; //temp is used to store the value
-        for (int i = 2; i <= num - 1; i++){ // Check number is divisible by other number or not
-            if (num % i == 0){
+        int temp = 0; // temp is used to store the value
+        for (int i = 2; i <= num - 1; i++) { // Check number is divisible by other number or not
+            if (num % i == 0) {
                 temp = temp + 1;
             }
-        } 
-        //Check temp value for Prime Number
-        if (temp > 0){
+        }
+        // Check temp value for Prime Number
+        if (temp > 0) {
             System.out.println(num + " is not prime Number ");
         } else {
             System.out.println(num + " is Prime Number ");
         }
         sc.close();
     }
+
     public static void main(String[] args) {
         primeNumber();
     }
 }
-
