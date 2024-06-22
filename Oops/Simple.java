e488e09855bddb9f0c51440645caafd00e40b24c
Faisal-Khann <faisalkhanfaisal919@gmail.com>
2024-06-23 00:03:00 +0530

    git add -A

---
+++ Oops/Simple.java
@@ -0,0 +1,29 @@
+package Oops;
+
+class Animal {
+    void eat() {
+        System.out.println("Eating....");
+    }
+
+    void run() {
+        System.out.println("Running....");
+    }
+}
+
+class Dog extends Animal{
+    void bark(){
+        System.out.println("Barking....");
+    }
+
+    //method override
+    void run(){
+        System.out.println("Running Fast...");
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+        Dog d = new Dog();
+        d.run();
+        d.bark();
+    }   
+}
