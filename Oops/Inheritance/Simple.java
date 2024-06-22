65ab2abc5d27efb1e68ee75314115593be40b218
Faisal-Khann <faisalkhanfaisal919@gmail.com>
2024-06-23 00:18:55 +0530

    git push -A

---
+++ Oops/Inheritance/Simple.java
@@ -0,0 +1,34 @@
+package Oops.Inheritance;
+// When a class inherits another class, it is known as a single inheritance.
+//In the example given below, Dog class inherits the Animal class, 
+//so there is the single inheritance.
+class Animal {
+    void eat() {
+        System.out.println("Eating....");
+    }
+
+    void run() {
+        System.out.println("Running....");
+    }
+}
+
+class Dog extends Animal{
+    void bark(){
+        System.out.println("Barking....");
+    }
+
+    //method override
+    void run(){
+        System.out.println("Running Fast...");
+    }
+}
+public class Simple {
+    public static void main(String[] args) {
+        Dog d = new Dog();
+        d.run();
+        d.bark();
+    }   
+}
+//javac Oops/Simple.java
+
+
