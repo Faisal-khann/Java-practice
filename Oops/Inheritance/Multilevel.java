83ba14016f54754c97090b396c57b0a7aa945f89
Faisal khan <faisalkhanfaisal496@gmail.com>
2024-07-28 12:53:02 +0530

    git add -A

--- Oops/Inheritance/Multilevel.java
+++ Oops/Inheritance/Multilevel.java
@@ -1,7 +1,7 @@
 package Oops.Inheritance;
 //When there is a chain of inheritance, it is known as multilevel inheritance. 
 //As you can see in the example given below, BabyDog class inherits the Dog class 
-//which again inherits the Animal class, so there is a multilevel inheritance.
+//which again inherits the Animal class, so there is a multilevel inheritance
 class Animal{
     void eat(){
         System.out.println("eating....");
